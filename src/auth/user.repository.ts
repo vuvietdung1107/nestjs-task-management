9f4380b2ba6183c0e4b545edcfaeb1fb7285ed33
dungvv <vuvietdung1107@gmail.com>
2020-09-11 23:26:01 +0700

    authentication

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,44 @@
 import { EntityRepository, Repository } from 'typeorm';
-import { User } from './usser.entity';
+import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+
+    const user = new User();
+    user.username = username;
+    user.password = await this.hashPassword(password, salt);
+    user.salt = salt;
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === 'ER_DUP_ENTRY') {
+        throw new ConflictException('Username already conflict');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    const { username, password } = authCredentialsDto;
+    const user = await this.findOne({ username });
+
+    if (user && await user.validatePassword(password)) {
+      return user.username;
+    } else {
+      return null;
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
+  }
 }
